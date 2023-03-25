a02bef9d310da943989efe8949dce7667ca55994
Asadbek <asadbekn443@gmail.com>
2023-03-25 16:41:53 -0400

    Upload image

--- recipe_tinder/components/GetIngredients.js
+++ recipe_tinder/components/GetIngredients.js
@@ -1,21 +1,45 @@
-import { StyleSheet, Text, View, Button,Pressable,Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Pressable, Dimensions, Image } from 'react-native';
+const ImagePicker = require('react-native-image-picker');
 
 export default function GetIngredients({ navigation }) {
-  
+  const [imageUri, setImageUri] = useState(null);
+
+  const handleChooseImage = () => {
+    ImagePicker.launchImageLibrary({
+      mediaType: 'photo',
+      includeBase64: false,
+      maxHeight: 200,
+      maxWidth: 200,
+    },
+    (response) => {
+      console.log(response);
+      this.setState({
+        resourcePath: response
+      });
+    },
+  )
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Ingridients Here</Text>
+      <Text style={styles.title}>Ingredients Here</Text>
+
+      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      
+      <Pressable onPress={handleChooseImage} style={styles.button}>
+        <Text style={styles.button_text}>Choose Image</Text>
+      </Pressable>
 
       <Pressable onPress={() => {
         navigation.replace("GetIngredientsList")
       }} style={styles.button}>
         <Text style={styles.button_text}>Go to the List</Text>
       </Pressable>
-      <View style={styles.button_container}>
-      </View>
     </View>
   );
 }
+
 const { height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
@@ -23,7 +47,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000',
     flex: 1,
     paddingTop: height*0.1,
-    paddingHorizontal: 40
+    paddingHorizontal: 40,
+    alignItems: 'center'
   },
   title: {
     fontSize: 40,
@@ -32,9 +57,6 @@ const styles = StyleSheet.create({
     letterSpacing: 0.25,
     color: '#03c2fc',
   },
-  button_container: {
-    marginTop: height*0.6
-  },
   button: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -42,7 +64,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 10,
     elevation: 3,
-    backgroundColor: '#03c2fc'
+    backgroundColor: '#03c2fc',
+    marginTop: 20
   },
   button_text: {
     fontSize: 16,
@@ -50,5 +73,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'white',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginTop: 20
   }
 });
