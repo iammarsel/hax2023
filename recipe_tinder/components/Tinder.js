206e62d4197799f865283a28e209a15c8cabccef
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2023-03-26 04:29:45 -0400

    fixing commit issues

--- recipe_tinder/components/Tinder.js
+++ recipe_tinder/components/Tinder.js
@@ -1,71 +1,189 @@
-import { StyleSheet, Text, View, Button,Pressable,Dimensions } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity, Pressable } from 'react-native';
 
-export default function Tinder({ navigation }) {
-  
+const images = [
+
+  {
+
+    uri: 'https://cdn.sanity.io/images/ordgikwe/production/3b881f9bcf37a73482e7dcc13fc5b94665256682-1600x1200.jpg?w=768&h=576&auto=format',
+
+    details: 'Image 1 Details'
+
+  },
+
+  {
+
+    uri: 'https://d33wubrfki0l68.cloudfront.net/487cd2effeeccb6540871a7f18d1d1e5c9b381d3/d0cfc/docs/assets/native-modules-android-open-project.png',
+
+    details: 'Image 2 Details'
+
+  },
+
+  {
+
+    uri: 'https://assets.toptal.io/images?url=https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1129718/retina_500x200_cover-cold-dive-into-react-native-a-beginners-tutorial-922a625efe84a4c2d782343b333b0bdb.png',
+
+    details: 'Image 3 Details'
+
+  },
+
+];
+
+export default function Tinder() {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const [imageList, setImageList] = useState(images);
+
+  const onRemoveImage = (index) => {
+
+    const newList = imageList.filter((item, i) => i !== index);
+
+    setImageList(newList);
+
+  };
+
+  const onAddImage = (index) => {
+
+    const newList = imageList.filter((item, i) => i !== index);
+
+    setSelectedImage(imageList[index]);
+
+    setImageList(newList);
+
+  };
+
+  const renderImageList = () => {
+
+    return imageList.map((item, index) => {
+
+      return (
+
+        <View style={styles.imageContainer} key={index}>
+
+          <TouchableOpacity onPress={() => setSelectedImage(item)}>
+
+            <Image source={{ uri: item.uri }} style={styles.image} />
+
+          </TouchableOpacity>
+
+          <Text style={styles.details}>{item.details}</Text>
+
+          <TouchableOpacity style={styles.removeButton} onPress={() => onRemoveImage(index)}>
+
+            <Text style={styles.removeButtonText}>Remove</Text>
+
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.addButton} onPress={() => onAddImage(index)}>
+
+            <Text style={styles.addButtonText}>Add to another page</Text>
+
+          </TouchableOpacity>
+
+        </View>
+
+      );
+
+    });
+
+  };
+
   return (
+
     <View style={styles.container}>
+
+      <ScrollView contentContainerStyle={styles.scrollView}>
+
+        {renderImageList()}
+
+      </ScrollView>
+
+      {selectedImage && (
+
+        <View style={styles.selectedImageContainer}>
+
+          <Image source={{ uri: selectedImage.uri }} style={styles.selectedImage} />
+
+          <Text style={styles.details}>{selectedImage.details}</Text>
+
+        </View>
+
+      )}
+
       <Text style={styles.title}>Welcome</Text>
 
       <Pressable onPress={() => {
+
         navigation.navigate("GetIngredients")
+
       }} style={styles.button}>
+
         <Text style={styles.button_text}>Go to Tinder</Text>
+
       </Pressable>
+
       <View style={styles.box1} />
+
       <Pressable onPress={() => {
+
         navigation.navigate("GetIngredients")
+
       }} style={styles.button}>
+
         <Text style={styles.button_text}>Go to Tinder</Text>
+
       </Pressable>
+
       <View style={styles.button_container}>
+
       </View>
 
-      
     </View>
-    
+
   );
+
 }
-const { height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
+
   container: {
-    backgroundColor: '#000000',
+
     flex: 1,
-    paddingTop: height*0.1,
-    paddingHorizontal: 40
+
+    paddingTop: 20,
+
+    backgroundColor: '#fff',
+
   },
-  box1:{
-    paddingTop:300,
-    marginTop:30,
-    borderRadius:20,
-    backgroundColor:'#03c2fc'
-  },
-  title: {
-    fontSize: 40,
-    lineHeight: 40,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: '#03c2fc',
-  },
-  button_container: {
-    marginTop: height*0.6
-  },
-  button: {
+
+  scrollView: {
+
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 10,
-    elevation: 3,
-    backgroundColor: '#03c2fc'
+
   },
-  button_text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  }
-});
+
+  imageContainer: {
+
+    width: '100%',
+
+    alignItems: 'center',
+
+    marginBottom: 20,
+
+  },
+
+  image: {
+
+    width: 200,
+
+    height: 200,
+
+    resizeMode: 'cover',
+
+    borderRadius: 10,
+
+  },
+
+})
